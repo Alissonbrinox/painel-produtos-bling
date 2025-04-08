7cb008d413e47d79ffdb8ce00b284159132829fc
Alissonbrinox <alisson.zavarize@grupobrinox.com.br>
2025-04-08 14:16:21 -0300

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import time
 client_id = "9838ab2d65a8f74ab1c780f76980272dd66dcfb9"
 client_secret = "a1ffcf45d3078aaffab7d0746dc3513d583a432277e41ca80eff03bf7275"
 refresh_token = "3fb1cde76502690d170d309fab20f48e5c22b71e"
-authorization_code = "8c1a9e382e008552f3a9096fc4376a4beaa21acf"
+authorization_code = "789f76867d6f6e5f097ef2e3231078381138845c"
 
 # =================== TOKEN ===================
 def refresh_access_token(refresh_token):
