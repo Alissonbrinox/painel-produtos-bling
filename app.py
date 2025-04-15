c781fb70cff096e7418e55c9f98a10ec5152a47a
Alissonbrinox <alisson.zavarize@grupobrinox.com.br>
2025-04-15 12:22:02 -0300

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # =================== CONFIGURAÇÕES ===================
 client_id = "9838ab2d65a8f74ab1c780f76980272dd66dcfb9"
 client_secret = "a1ffcf45d3078aaffab7d0746dc3513d583a432277e41ca80eff03bf7275"
-authorization_code = "6b4402ce0dc1b576209ca9d1049a31761020207f"
+authorization_code = "529aa42df397d7482cb59905b1a427e8e3f19f4f"
 
 if "refresh_token" not in st.session_state:
     st.session_state["refresh_token"] = "3fb1cde76502690d170d309fab20f48e5c22b71e"
