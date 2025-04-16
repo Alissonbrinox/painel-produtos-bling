6dd0ba945eee5590de8e620c893f1937c94e47e6
Alissonbrinox <alisson.zavarize@grupobrinox.com.br>
2025-04-16 12:00:32 -0300

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import json
 # =================== CONFIGURAÇÕES ===================
 client_id = "9838ab2d65a8f74ab1c780f76980272dd66dcfb9"
 client_secret = "a1ffcf45d3078aaffab7d0746dc3513d583a432277e41ca80eff03bf7275"
-authorization_code = "7cf46d8578caf63f473fb933b44f5c8a7a8d2b64"
+authorization_code = "8bf68b010887fcc1042de7a92fc2ca626a075f28"
 
 if "refresh_token" not in st.session_state:
     st.session_state["refresh_token"] = "3fb1cde76502690d170d309fab20f48e5c22b71e"
