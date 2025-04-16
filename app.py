ce6c14a7ef191074ab1012d3de2d8aa7f444e1af
Alissonbrinox <alisson.zavarize@grupobrinox.com.br>
2025-04-16 12:09:29 -0300

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import json
 # =================== CONFIGURAÇÕES ===================
 client_id = "9838ab2d65a8f74ab1c780f76980272dd66dcfb9"
 client_secret = "a1ffcf45d3078aaffab7d0746dc3513d583a432277e41ca80eff03bf7275"
-authorization_code = "914fca2ce0a39b33cb23d8d9bf02750ddf2d19e2"
+authorization_code = "4c5b345b51c606ad16100655b1dc60b3a0595aa6"
 
 if "refresh_token" not in st.session_state:
     st.session_state["refresh_token"] = "3fb1cde76502690d170d309fab20f48e5c22b71e"
@@ -116,6 +116,7 @@ def mostrar_pedidos(pedidos):
         })
 
     df = pd.DataFrame(registros)
+    df["ID"] = df["ID"].astype(str)  # manter como texto para evitar notação científica
     st.dataframe(df, use_container_width=True)
 
     output = BytesIO()
